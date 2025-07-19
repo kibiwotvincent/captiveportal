253b2c4fbdf3ab5a646820bf454ba9dbb43acc11
Kibiwot Vincent <kibiwotvincent@gmail.com>
2025-07-19 15:15:49 +0000

    update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     header("Expires: 0");
     header("Cache-Control: no-cache, no-store, must-revalidate");
     header("Pragma: no-cache");
-    header("Connection: close");
+    //header("Connection: close");
 
     // URL of your online login handler
     $onlineServer = "https://offylink.bytesbay.site/";
@@ -12,9 +12,12 @@
         // Construct current URL to send as a return parameter
         $currentUrl = $_SERVER['REQUEST_SCHEME'] . "://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
         $encodedReturn = urlencode($currentUrl);
+        $url = "https://offylink.bytesbay.site/prep-hotspot-login?r=http%3A%2F%2F192.168.1.1%3A8002%2Findex.php%3Fzone%3Dbytesbay1%26redirurl%3Dhttp%253A%252F%252Fconnectivitycheck.gsta";
 
         // Redirect to the online login page
-        header("Location: " . $onlineServer . "prep-hotspot-login?r=" . $encodedReturn);
+        //header("Location: " . $onlineServer . "prep-hotspot-login?r=" . $encodedReturn);
+        header("Location: " . $url);
+      
         exit;
     }
 
@@ -34,8 +37,8 @@
 
     <!-- Hidden form for pfSense captive portal -->
     <form method="post" action="$PORTAL_ACTION$" id="login_form">
-        <input type="hidden" name="auth_user" value="<?= $username ?>">
-        <input type="hidden" name="auth_pass" value="<?= $password ?>">
+        <input type="hidde" name="auth_user" value="<?= $username ?>">
+        <input type="hidde" name="auth_pass" value="<?= $password ?>">
         <input type="hidden" name="auth_voucher" value="">
         <input type="hidden" name="redirurl" value="$PORTAL_REDIRURL$">
         <input type="hidden" name="zone" value="$PORTAL_ZONE$">
@@ -44,7 +47,7 @@
 
     <script>
         window.onload = function() {
-            document.getElementById("login_form_submit").click();
+            //document.getElementById("login_form_submit").click();
         };
     </script>
 </body>
